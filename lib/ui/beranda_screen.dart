d043c67b12ecc261d41f2c8b3a034e2a028bbe50
agung851 <agung.sasongko@bsi.ac.id>
2021-12-10 17:52:01 +0700

    buat ui beranda screen

---
+++ lib/ui/beranda_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BerandaScreen extends StatelessWidget {
+  const BerandaScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Ini Halaman Beranda'),
+      ),
+    );
+  }
+}
