b71a0620b942401b5338c6aa858b65c561b6319c
agung851 <agung.sasongko@bsi.ac.id>
2021-12-13 06:21:52 +0700

    ubah isi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:contoh1/bloc/beranda_bloc.dart';
 import 'package:contoh1/ui/about_screen.dart';
 import 'package:contoh1/ui/beranda_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: AboutScreen(),
+  runApp(  MaterialApp(
+    home: MultiBlocProvider(
+       providers: [
+          BlocProvider<MangaBloc>(create: (c)=>MangaBloc())
+       ],
+       child: BerandaScreen(),
+    ),
     title: 'Contoh 1',
     debugShowCheckedModeBanner: false,
   ));
